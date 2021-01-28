05d1561adb04681269e8b0c5a63dcc0bf80002fe
Benis Tambe <will.jenifer@yahoo.com>
2021-01-28 00:21:37 -0500

    commit tree 598f1ea7cf31861a0a6c5e10ee23490d44a29b62

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    arr = self.split(/\.|\?|!/)
+    arr.each.with_index(1) do |x, index|
+      if x == ""
+        arr.delete_at(index)
+      end
+    end
+    arr.count
   end
+
 end
